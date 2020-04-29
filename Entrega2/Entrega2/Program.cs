28769d61d772cceef0dbbe5c21d969d23c37b2c4
rcorrea4 <rcorrea4@miuandes.cl>
2020-04-29 16:09:59 -0400

    commit WMPLib

--- Entrega2/Entrega2/Program.cs
+++ Entrega2/Entrega2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMPLib;
 
 namespace Entrega2
 {
@@ -10,6 +11,44 @@ namespace Entrega2
     {
         static void Main(string[] args)
         {
+
+            WindowsMediaPlayer sonido;
+            
+
+            try
+            {
+                sonido = new WindowsMediaPlayer();
+             
+
+
+                sonido.URL = @"C:\Users\corre\OneDrive\Escritorio\P.O.O. 20-01\Safaera-BadBunny.mp3";
+                sonido.controls.play();
+                
+                string n = null;
+                while (n != "5")
+                {
+                    Console.WriteLine("1-pausa 2-contiuar 5-salir");
+                    n = Console.ReadLine();
+                    if (n == "1")
+                    {
+                        sonido.controls.pause();
+
+                    }
+                    if (n == "2")
+                    {
+                        double tiempo = sonido.controls.currentPosition;
+                        sonido.controls.currentPosition = tiempo;
+                        sonido.controls.play();
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error. " + ex.Message);
+            }
+
         }
     }
 }
