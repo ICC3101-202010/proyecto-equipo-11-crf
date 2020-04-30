91caa8973968be2c79d8da04bba7d63d7e92cc70
rcorrea4 <rcorrea4@miuandes.cl>
2020-04-30 11:06:47 -0400

    commit raimundo clases

---
+++ Entrega2/Entrega2/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2
+{
+    class Persona
+    {
+        private string namePerson;
+        private string lastName;
+        private string rut;
+        private string nation;
+        private DateTime birthDate;
+        private string gender;
+
+        public string NamePerson { get => namePerson; set => namePerson = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Rut { get => rut; set => rut = value; }
+        public string Nacion { get => nation; set => nation = value; }
+        public DateTime BirthDate { get => birthDate; set => birthDate = value; }
+        public string Gender { get => gender; set => gender = value; }
+
+        public Persona(string namePerson, string lastName, string rut, string nation, DateTime birthDate, string gender)
+        {
+            this.namePerson = namePerson;
+            this.lastName = lastName;
+            this.rut = rut;
+            this.nation = nation;
+            this.birthDate = birthDate;
+            this.gender = gender;
+        }
+    }
+}
---
+++ Entrega2/Entrega2/Video.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2
+{
+    class Video
+    {
+        private string nameVideo;
+        private string genero;
+        private int vistos;
+        private List<actor> actores;
+        private director director;
+        private float duracion;
+        private string estudio;
+        private int tamanoMemoria;
+        private string direccionMemoria;
+        private string caratula;
+    }
+}
