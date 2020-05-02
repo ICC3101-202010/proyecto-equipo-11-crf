4ae0106ca83f2a206876835e44b2e76f24d6ff75
unknown <fjmiranda4iuandes.cl>
2020-05-02 02:24:01 -0400

    all classes

--- Entrega2/Entrega2/Musico.cs
+++ Entrega2/Entrega2/Musico.cs
@@ -5,15 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 namespace Entrega2
 {
-    class Musico:Persona
-    {
-        private string Rol;
-        private Banda Banda;
+     class Musico : Persona
+     {
+        private string rol;
+        private Banda banda;
         private List<Cancion> canciones;
 
 
-        public string Rol1 { get => Rol; set => Rol = value; }
-
-        public Musico(string Rol) => this.Rol = Rol;
-    }
+        public string Rol { get => rol; set => rol = value; }
+        public Banda Banda { get => banda; set => banda = value; }
+        public List<Cancion> Canciones { get => canciones; }
+        //string rol, Banda banda, List<Cancion> cancion,
+        public Musico(string rol, Banda banda, List<Cancion> cancion, string namePerson, string lastName, string rut, string nation, DateTime birthDate, string gender) : base(namePerson, lastName, rut, nation, birthDate, gender)
+        {   this.rol = rol;
+            this.banda = banda;
+            this.canciones = cancion;
+        }
+     }
 }
