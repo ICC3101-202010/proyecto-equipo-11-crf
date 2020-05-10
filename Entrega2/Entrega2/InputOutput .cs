8ff71925ba76b98f108d4c6baefe8e5d2f2f1637
unknown <fjmiranda4iuandes.cl>
2020-05-10 17:40:54 -0400

    LAUNCHER created

--- Entrega2/Entrega2/InputOutput .cs
+++ Entrega2/Entrega2/InputOutput .cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entrega2
+namespace Entrega2.IO
 {
-    class InputOutput
+    public static class InputOutput
     {
-       
+        public static string ShowOptions(List<string> options)
+        {
+            int i = 0;
+            Console.WriteLine("\n\nSelecciona una opcion:");
+            foreach (string option in options)
+            {
+                Console.WriteLine(Convert.ToString(i) + ". " + option);
+                i += 1;
+            }
+            return options[Convert.ToInt16(Console.ReadLine())];
+        }
     }
 }
---
+++ Entrega2/Entrega2/LAUNCHER-Spotlix.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entrega2.IO;
+
+namespace Entrega2
+{
+    class LAUNCHER_Spotlix
+    {
+        public LAUNCHER_Spotlix() 
+        {
+            // LIST OF Loging
+            Loginlist = new List<string>()
+            {
+                "Registarse",
+                "Login",
+                //"Cambiar contraseña",
+                "Salir",
+
+            };
+
+ 
+        }
+        public List<string> Loginlist { get; set; }
+
+        public void Spotlix() 
+        {
+            RegistroUsuarios database = new RegistroUsuarios();
+            Server server = new Server(database);
+            EnvioMail mailSender = new EnvioMail();
+            EnvioSMS smsSender = new EnvioSMS();
+            Usuario user = new Usuario();
+            
+            server.Registered += mailSender.OnRegistered;
+
+            server.PasswordChanged += mailSender.OnPasswordChanged;
+
+            server.PasswordChanged += smsSender.OnPasswordChanged;
+
+            mailSender.EmailSent += user.OnEmailSent;
+
+            user.EmailVerified += server.OnEmailVerified;
+
+            bool exec = true;
+            while (exec)
+            {
+
+                string chosen = InputOutput.ShowOptions(new List<string>() { "Registrarse", "Log in", "Cambiar contrasena", "Salir" });
+                switch (chosen)
+                {
+                    case "Registrarse":
+                        Console.Clear();
+                        server.Registrarse();
+                        break;
+                    case "Log in":
+                        Console.Clear();
+                        server.LogIn();
+                        break;
+                    case "Cambiar contrasena":
+                        Console.Clear();
+                        server.CambiarContrasena();
+                        break;
+                    case "Salir":
+                        exec = false;
+                        break;
+                }
+
+                Console.Clear();
+            }
+
+        }
+
+
+
+
+    }
+
+}
