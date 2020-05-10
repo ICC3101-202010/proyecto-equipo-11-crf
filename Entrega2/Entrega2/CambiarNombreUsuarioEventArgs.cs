27ddba104206b54ec34a788d5735a7bb6c656cb3
Carlo Vitali <cvitali@miuandes.cl>
2020-05-10 19:23:17 -0400

    1

---
+++ Entrega2/Entrega2/CambiarNombreUsuarioEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Entrega2
+{
+    public class CambiarNombreUsuarioEventArgs : EventArgs
+    {
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
