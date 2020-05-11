7b019d5561129c24b734ee83f725a9a748a4651b
rcorrea4 <rcorrea4@miuandes.cl>
2020-05-10 23:05:25 -0400

    c

--- Entrega2/Entrega2/LAUNCHER-Spotlix.cs
+++ Entrega2/Entrega2/LAUNCHER-Spotlix.cs
@@ -67,13 +67,12 @@ namespace Entrega2
                         string server1=server.LogIn();
                         if (server1 == null)
                         {
-<<<<<<< HEAD
-                            //asignar usuario
+
                             Console.WriteLine("Interfaz Spotflix ");
-=======
+
                             
                             Console.Write("Interfaz Spotflix ");
->>>>>>> 2318e8200606dab4279a5aff0d05b27476866050
+
 
                             int a = 0;
                             while (a != 5)
@@ -89,6 +88,7 @@ namespace Entrega2
                                 {
                                     Console.WriteLine("Buscar");
                                     Console.WriteLine("Prueba prueba");
+                                    //Algo
                                 }
 
                                 else if (a == 3)
