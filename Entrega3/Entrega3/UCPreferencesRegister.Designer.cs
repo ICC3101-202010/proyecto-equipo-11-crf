d0eac6e88ac382ab9f59849ab2ff238559b8f941
unknown <fjmiranda4iuandes.cl>
2020-05-30 19:09:46 -0400

    fixed error

---
+++ Entrega3/Entrega3/UCPreferencesRegister.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Entrega3
+{
+    partial class UCPreferencesRegister
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1rstPreference = new System.Windows.Forms.Label();
+            this.label2ndPreference = new System.Windows.Forms.Label();
+            this.labelMobileRlabel3thPreferenceRegister = new System.Windows.Forms.Label();
+            this.PanelRegisterInformation = new System.Windows.Forms.Panel();
+            this.textBox3Preference = new System.Windows.Forms.TextBox();
+            this.textBox2reference = new System.Windows.Forms.TextBox();
+            this.textBox1Preference = new System.Windows.Forms.TextBox();
+            this.labelRegistrationPreferencesRegister = new System.Windows.Forms.Label();
+            this.btnSubmitPreferencesRegister = new System.Windows.Forms.Button();
+            this.PanelRegisterInformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1rstPreference
+            // 
+            this.label1rstPreference.AutoSize = true;
+            this.label1rstPreference.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1rstPreference.Location = new System.Drawing.Point(169, 105);
+            this.label1rstPreference.Name = "label1rstPreference";
+            this.label1rstPreference.Size = new System.Drawing.Size(67, 13);
+            this.label1rstPreference.TabIndex = 0;
+            this.label1rstPreference.Text = "Enter 1 Artist";
+            // 
+            // label2ndPreference
+            // 
+            this.label2ndPreference.AutoSize = true;
+            this.label2ndPreference.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2ndPreference.Location = new System.Drawing.Point(169, 151);
+            this.label2ndPreference.Name = "label2ndPreference";
+            this.label2ndPreference.Size = new System.Drawing.Size(67, 13);
+            this.label2ndPreference.TabIndex = 1;
+            this.label2ndPreference.Text = "Enter 1 Artist";
+            // 
+            // labelMobileRlabel3thPreferenceRegister
+            // 
+            this.labelMobileRlabel3thPreferenceRegister.AutoSize = true;
+            this.labelMobileRlabel3thPreferenceRegister.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.labelMobileRlabel3thPreferenceRegister.Location = new System.Drawing.Point(169, 204);
+            this.labelMobileRlabel3thPreferenceRegister.Name = "labelMobileRlabel3thPreferenceRegister";
+            this.labelMobileRlabel3thPreferenceRegister.Size = new System.Drawing.Size(67, 13);
+            this.labelMobileRlabel3thPreferenceRegister.TabIndex = 2;
+            this.labelMobileRlabel3thPreferenceRegister.Text = "Enter 1 Artist";
+            // 
+            // PanelRegisterInformation
+            // 
+            this.PanelRegisterInformation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(22)))), ((int)(((byte)(39)))));
+            this.PanelRegisterInformation.Controls.Add(this.btnSubmitPreferencesRegister);
+            this.PanelRegisterInformation.Controls.Add(this.labelRegistrationPreferencesRegister);
+            this.PanelRegisterInformation.Controls.Add(this.textBox3Preference);
+            this.PanelRegisterInformation.Controls.Add(this.textBox2reference);
+            this.PanelRegisterInformation.Controls.Add(this.textBox1Preference);
+            this.PanelRegisterInformation.Controls.Add(this.labelMobileRlabel3thPreferenceRegister);
+            this.PanelRegisterInformation.Controls.Add(this.label2ndPreference);
+            this.PanelRegisterInformation.Controls.Add(this.label1rstPreference);
+            this.PanelRegisterInformation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelRegisterInformation.Location = new System.Drawing.Point(0, 0);
+            this.PanelRegisterInformation.Name = "PanelRegisterInformation";
+            this.PanelRegisterInformation.Size = new System.Drawing.Size(545, 385);
+            this.PanelRegisterInformation.TabIndex = 7;
+            // 
+            // textBox3Preference
+            // 
+            this.textBox3Preference.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox3Preference.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3Preference.Location = new System.Drawing.Point(281, 197);
+            this.textBox3Preference.Name = "textBox3Preference";
+            this.textBox3Preference.Size = new System.Drawing.Size(126, 20);
+            this.textBox3Preference.TabIndex = 5;
+            // 
+            // textBox2reference
+            // 
+            this.textBox2reference.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox2reference.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2reference.Location = new System.Drawing.Point(281, 149);
+            this.textBox2reference.Name = "textBox2reference";
+            this.textBox2reference.Size = new System.Drawing.Size(126, 20);
+            this.textBox2reference.TabIndex = 4;
+            // 
+            // textBox1Preference
+            // 
+            this.textBox1Preference.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox1Preference.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1Preference.Location = new System.Drawing.Point(281, 98);
+            this.textBox1Preference.Name = "textBox1Preference";
+            this.textBox1Preference.Size = new System.Drawing.Size(126, 20);
+            this.textBox1Preference.TabIndex = 3;
+            // 
+            // labelRegistrationPreferencesRegister
+            // 
+            this.labelRegistrationPreferencesRegister.Font = new System.Drawing.Font("Lucida Console", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegistrationPreferencesRegister.ForeColor = System.Drawing.Color.White;
+            this.labelRegistrationPreferencesRegister.Location = new System.Drawing.Point(82, 16);
+            this.labelRegistrationPreferencesRegister.MaximumSize = new System.Drawing.Size(900, 900);
+            this.labelRegistrationPreferencesRegister.Name = "labelRegistrationPreferencesRegister";
+            this.labelRegistrationPreferencesRegister.Size = new System.Drawing.Size(382, 43);
+            this.labelRegistrationPreferencesRegister.TabIndex = 16;
+            this.labelRegistrationPreferencesRegister.Text = "Registration";
+            this.labelRegistrationPreferencesRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSubmitPreferencesRegister
+            // 
+            this.btnSubmitPreferencesRegister.BackColor = System.Drawing.Color.Lavender;
+            this.btnSubmitPreferencesRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitPreferencesRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnSubmitPreferencesRegister.Location = new System.Drawing.Point(210, 267);
+            this.btnSubmitPreferencesRegister.Name = "btnSubmitPreferencesRegister";
+            this.btnSubmitPreferencesRegister.Size = new System.Drawing.Size(127, 23);
+            this.btnSubmitPreferencesRegister.TabIndex = 17;
+            this.btnSubmitPreferencesRegister.Text = "Submit";
+            this.btnSubmitPreferencesRegister.UseVisualStyleBackColor = false;
+            this.btnSubmitPreferencesRegister.Click += new System.EventHandler(this.btnSubmitPreferencesRegister_Click);
+            // 
+            // UCPreferencesRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PanelRegisterInformation);
+            this.Name = "UCPreferencesRegister";
+            this.Size = new System.Drawing.Size(545, 385);
+            this.PanelRegisterInformation.ResumeLayout(false);
+            this.PanelRegisterInformation.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1rstPreference;
+        private System.Windows.Forms.Label label2ndPreference;
+        private System.Windows.Forms.Label labelMobileRlabel3thPreferenceRegister;
+        private System.Windows.Forms.Panel PanelRegisterInformation;
+        private System.Windows.Forms.TextBox textBox3Preference;
+        private System.Windows.Forms.TextBox textBox2reference;
+        private System.Windows.Forms.TextBox textBox1Preference;
+        private System.Windows.Forms.Label labelRegistrationPreferencesRegister;
+        private System.Windows.Forms.Button btnSubmitPreferencesRegister;
+    }
+}
