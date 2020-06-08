2d3f7f619e5fa6240825b9c224817b68c981c800
unknown <fjmiranda4iuandes.cl>
2020-06-08 00:25:52 -0400

    library list not working

--- SporflixWF/SporflixWF/Library.Designer.cs
+++ SporflixWF/SporflixWF/Library.Designer.cs
@@ -33,8 +33,16 @@
             this.panelLibrary = new System.Windows.Forms.Panel();
             this.labelEmptyLibrary = new System.Windows.Forms.Label();
             this.pbEmpty = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelAddPlaylist = new System.Windows.Forms.Label();
             this.panelLibrary.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbEmpty)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panelLibraruTitle
@@ -48,6 +56,10 @@
             // 
             // panelLibrary
             // 
+            this.panelLibrary.AutoScroll = true;
+            this.panelLibrary.Controls.Add(this.labelAddPlaylist);
+            this.panelLibrary.Controls.Add(this.pictureBox2);
+            this.panelLibrary.Controls.Add(this.flowLayoutPanel1);
             this.panelLibrary.Controls.Add(this.labelEmptyLibrary);
             this.panelLibrary.Controls.Add(this.pbEmpty);
             this.panelLibrary.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -61,7 +73,7 @@
             // 
             this.labelEmptyLibrary.AutoSize = true;
             this.labelEmptyLibrary.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.labelEmptyLibrary.Location = new System.Drawing.Point(265, 237);
+            this.labelEmptyLibrary.Location = new System.Drawing.Point(258, 217);
             this.labelEmptyLibrary.Name = "labelEmptyLibrary";
             this.labelEmptyLibrary.Size = new System.Drawing.Size(127, 13);
             this.labelEmptyLibrary.TabIndex = 1;
@@ -71,12 +83,59 @@
             // pbEmpty
             // 
             this.pbEmpty.Image = ((System.Drawing.Image)(resources.GetObject("pbEmpty.Image")));
-            this.pbEmpty.Location = new System.Drawing.Point(203, 44);
+            this.pbEmpty.Location = new System.Drawing.Point(203, 24);
             this.pbEmpty.Name = "pbEmpty";
             this.pbEmpty.Size = new System.Drawing.Size(242, 190);
             this.pbEmpty.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbEmpty.TabIndex = 0;
             this.pbEmpty.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(194, 211);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 158);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(250, 248);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 38);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // labelAddPlaylist
+            // 
+            this.labelAddPlaylist.AutoSize = true;
+            this.labelAddPlaylist.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.labelAddPlaylist.Location = new System.Drawing.Point(325, 259);
+            this.labelAddPlaylist.Name = "labelAddPlaylist";
+            this.labelAddPlaylist.Size = new System.Drawing.Size(60, 13);
+            this.labelAddPlaylist.TabIndex = 4;
+            this.labelAddPlaylist.Text = "Add playlist";
             // 
             // Library
             // 
@@ -87,9 +146,14 @@
             this.Controls.Add(this.panelLibraruTitle);
             this.Name = "Library";
             this.Size = new System.Drawing.Size(643, 360);
+            this.Enter += new System.EventHandler(this.Library_Enter);
             this.panelLibrary.ResumeLayout(false);
             this.panelLibrary.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbEmpty)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -100,5 +164,10 @@
         private System.Windows.Forms.Panel panelLibrary;
         private System.Windows.Forms.Label labelEmptyLibrary;
         private System.Windows.Forms.PictureBox pbEmpty;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelAddPlaylist;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
